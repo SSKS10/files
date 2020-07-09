15c08357c497e33c970ccff020ed0fdb424a0add
unknown <shreyaskhadke@gmail.com>
2020-07-09 14:42:51 +0530

    'start'

---
+++ voting.js
@@ -0,0 +1,33 @@
+var mysql = require('mysql');
+var express = require('express');
+var bodyParser = require('body-parser');
+const app     = express();
+app.use(bodyParser.urlencoded({ extended: false })); 
+app.use(bodyParser.json());
+app.use(express.static(__dirname));
+var con = mysql.createConnection({
+  host: "localhost",
+  user: "root",
+  password: "shreyas10",
+  database: "voting"
+});
+
+app.post('/for_vote',function(req, res) {
+	var json_data=req.body;
+	//console.log(json_data)
+	var result=[]
+	for(var i in json_data)
+    result.push(json_data [i]);
+    //console.log([result]);
+    con.connect(function(err) {
+		  if (err) throw err;
+		  var sql = "INSERT INTO votes (bat, bowl,field,emerge,fair,catch) VALUES ?";
+		  con.query(sql,[[result]], function (err, result) {
+		    if (err) throw err;
+		  });
+	});
+	return res.redirect('/first_page.html')
+})
+app.listen(3360, function() {
+  console.log('Server running at http://127.0.0.1:3360/');
+});
